9f1e1ecacb6c4ccaee39d4654774074bdfd9b0c5
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2025-01-16 17:46:59 +0200

    Frontend: Implemented Signup page right side structure design

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -11,6 +11,7 @@ import {
   MessageSquare,
   User,
 } from "lucide-react";
+import AuthImagePattern from "../components/AuthImagePattern";
 
 const SignUpPage = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -30,16 +31,16 @@ const SignUpPage = () => {
   };
 
   return (
-    <div className="min-h-screen grid lg-grid-cols-2">
+    <div className="min-h-screen grid lg:grid-cols-2">
       {/* left side */}
       <div className="flex flex-col justify-center items-center p-6 sm:p-12">
         <div className="w-full max-w-md space-y-8">
-          {/* Logo */}
+          {/* LOGO */}
           <div className="text-center mb-8">
             <div className="flex flex-col items-center gap-2 group">
               <div
-                className="size-12 rounded-xl bg-primary/10 flex items-center
-            justify-center group-hover:bg-primary/20 transition-colors"
+                className="size-12 rounded-xl bg-primary/10 flex items-center justify-center 
+              group-hover:bg-primary/20 transition-colors"
               >
                 <MessageSquare className="size-6 text-primary" />
               </div>
@@ -137,6 +138,7 @@ const SignUpPage = () => {
               )}
             </button>
           </form>
+
           <div className="text-center">
             <p className="text-base-content/60">
               Already have an account?{" "}
@@ -147,6 +149,13 @@ const SignUpPage = () => {
           </div>
         </div>
       </div>
+
+      {/* right side */}
+
+      <AuthImagePattern
+        title="Join our community"
+        subtitle="Connect with friends, share moments, and stay in touch with your loved ones."
+      />
     </div>
   );
 };
