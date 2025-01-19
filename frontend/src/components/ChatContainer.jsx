3d2a7db8221017b4d4c0e0450f7f7a52026ec3e8
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2025-01-19 18:49:50 +0200

    Frontend: Created chat header with user avatar, name, and online status

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,15 +1,18 @@
 import React, { useEffect } from "react";
 import { useChatStore } from "../store/useChatStore";
+import ChatHeader from "./ChatHeader.jsx";
+import MessageInput from "./MessageInput.jsx";
 
 const ChatContainer = () => {
   const { messages, getMessages, isMessagesLoading, selectedUser } =
     useChatStore();
 
+    useEffect(() => {
+      getMessages(selectedUser._id);
+    }, [selectedUser._id, getMessages]);
+
   if (isMessagesLoading) return <div>Loading...</div>;
 
-  useEffect(() => {
-    getMessages(selectedUser._id);
-  }, [selectedUser._id, getMessages]);
 
   return (
     <div className="flex-1 flex flex-col overflow-auto">
