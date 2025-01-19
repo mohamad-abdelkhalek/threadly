15e09122e5544d8c279a3c75be03eb64d3efcc76
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2025-01-19 18:58:18 +0200

    Frontend: Created message skeleton component for loading state

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -2,17 +2,24 @@ import React, { useEffect } from "react";
 import { useChatStore } from "../store/useChatStore";
 import ChatHeader from "./ChatHeader.jsx";
 import MessageInput from "./MessageInput.jsx";
+import MessageSkeleton from "./skeletons/MessageSkeleton.jsx";
 
 const ChatContainer = () => {
   const { messages, getMessages, isMessagesLoading, selectedUser } =
     useChatStore();
 
-    useEffect(() => {
-      getMessages(selectedUser._id);
-    }, [selectedUser._id, getMessages]);
+  useEffect(() => {
+    getMessages(selectedUser._id);
+  }, [selectedUser._id, getMessages]);
 
-  if (isMessagesLoading) return <div>Loading...</div>;
-
+  if (isMessagesLoading)
+    return (
+      <div className="flex-1 flex flex-col overflow-auto">
+        <ChatHeader />
+        <MessageSkeleton />
+        <MessageInput />
+      </div>
+    );
 
   return (
     <div className="flex-1 flex flex-col overflow-auto">
