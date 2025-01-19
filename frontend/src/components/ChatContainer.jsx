fd69dcb93f04d451f8123f5d00cbe8694e72aba3
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2025-01-19 18:22:17 +0200

    Added basic chat container structure

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useChatStore } from "../store/useChatStore";
 
 const ChatContainer = () => {
-  return (
-    <div>ChatContainer</div>
-  )
-}
+  const { messages, getMessages, isMessagesLoading, selectedUser } =
+    useChatStore();
 
-export default ChatContainer
+  if (isMessagesLoading) return <div>Loading...</div>;
+
+  useEffect(() => {
+    getMessages(selectedUser._id);
+  }, [selectedUser._id, getMessages]);
+
+  return (
+    <div className="flex-1 flex flex-col overflow-auto">
+      <ChatHeader />
+
+      <p>messages...</p>
+
+      <MessageInput />
+    </div>
+  );
+};
+
+export default ChatContainer;
